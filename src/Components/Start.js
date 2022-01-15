f572c946f03bec46a19fa925e75c2fd8d589d78c
Manahil Ameer <manahil1054@gmail.com>
2022-01-15 19:37:56 +0500

    Added Route to main page

--- src/Components/Start.js
+++ src/Components/Start.js
@@ -2,7 +2,18 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import "../styles/start.css";
 import MainNavbar from "./Navbar";
+import { useNavigate } from "react-router-dom";
+
+const Hello = () => {
+  const history = useNavigate();
+  const handleClick = () => history.push("/app");
+};
 function Start() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/app");
+    console.log(navigate);
+  };
   return (
     <>
       <MainNavbar></MainNavbar>
@@ -13,11 +24,13 @@ function Start() {
           {/* </div> */}
           <div className="subtitle">
             {/* <text> */}
-              Focus on what truly matters with Habitify. Build the best version
-              of yourself by mastering your habits.
+            Focus on what truly matters with Habitify. Build the best version of
+            yourself by mastering your habits.
             {/* </text> */}
           </div>
-          <div className="button-link">Get Started</div>
+          <div onClick={handleClick} className="button-link">
+            Get Started
+          </div>
         </div>
       </main>
     </>
