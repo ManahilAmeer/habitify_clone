af930ab1eba1be744fb4a37151b2892e8bbcdf59
Manahil Ameer <manahil1054@gmail.com>
2022-01-19 22:16:37 +0500

    Configuring firebase

---
+++ src/Components/firebase.js
@@ -0,0 +1,58 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZsGUnrUdoj9O6Yg6DpnXXsz0b8SYCoAI",
+  authDomain: "habitify-d9ebc.firebaseapp.com",
+  projectId: "habitify-d9ebc",
+  storageBucket: "habitify-d9ebc.appspot.com",
+  messagingSenderId: "213736257615",
+  appId: "1:213736257615:web:f8e18b82fdd88cb033923a",
+  measurementId: "G-NEQ76JYFNJ",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+const db = app.firestore();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await auth.signInWithPopup(googleProvider);
+    const user = res.user;
+    const query = await db
+      .collection("users")
+      .where("uid", "==", user.uid)
+      .get();
+    if (query.docs.length === 0) {
+      await db.collection("users").add({
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const sendPasswordResetEmail = async (email) => {
+  try {
+    await auth.sendPasswordResetEmail(email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const logout = () => {
+  auth.signOut();
+};
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  sendPasswordResetEmail,
+  logout,
+};
