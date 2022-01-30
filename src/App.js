0c57036861e203aa1834ee379b939ca1c364b7a4
Manahil Ameer <manahil1054@gmail.com>
2022-01-30 18:27:19 +0500

    Changing App and Index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
 import Landing from '@views/Landing';
+import SignIn from "@views/SignIn";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "@store/configureStore";
+import SplitPane from "@components/SplitPane";
 function App() {
-  
   return (
-    <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="/app" element={<SplitPane />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+      </Routes>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from '@store/configureStore';
-import SplitPane from "@components/SplitPane";
 import App from '@/App';
-import SignIn from "@views/SignIn"
-
 import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <Routes>
-          <Route path="/" element={<App />}></Route>
-          <Route path="/app" element={<SplitPane />}></Route>
-          <Route path="/sign-in" element={<SignIn />}></Route>
-        </Routes>
+        <App />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
