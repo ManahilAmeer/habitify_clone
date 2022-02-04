dc2394551e1a9c032bcad1702d5867132b035a5a
Manahil Ameer <manahil1054@gmail.com>
2022-02-04 17:37:58 +0500

    Form to edit/delete

---
+++ src/views/EditHabitForm/EditHabit.js
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from "react";
+import { Formik, Form, Field } from "formik";
+import PropTypes from "prop-types";
+import Goal from "@components/Goal/Goal";
+import { updateHabit,deleteHabit } from "@store/habitsReducer";
+import { useDispatch } from "react-redux";
+import QMark from "@assets/QMark.svg";
+import "@views/EditHabitForm/EditHabit.css";
+function EditHabit(props) {
+  const name=props.name;
+  const usergoal = props.goal;
+  const dispatch = useDispatch();
+  useEffect(() => {}, [updateHabit,deleteHabit]);
+  const [goal, setGoal] = useState(1);
+  const handleDeleteButton=()=>{
+    dispatch(deleteHabit({ID:props.ID}))
+  }
+  return (
+    <>
+      <div className="tab">
+        <Formik
+          initialValues={{ name: name, goal: usergoal }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.name) {
+              errors.name = "Required";
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            const data = {
+              name: values.name,
+              goal: values.goal,
+              ID: props.ID,
+            };
+            dispatch(updateHabit(data));
+            setSubmitting(false);
+            props.handleButton();
+          }}
+        >
+          {({ setFieldValue, isSubmitting }) => (
+            <Form>
+              <div className="tab-content">
+                <header className="content-header">Edit Habit</header>
+                <div className="create-form">
+                  <div className="name">
+                    <p className="name-text">NAME</p>
+                    <div className="name-input">
+                      <div className="input-grp">
+                        <Field
+                          type="name"
+                          name="name"
+                          autoComplete="off"
+                          onClick={() => {}}
+                          className="input"
+                        ></Field>
+                      </div>
+                      <div>
+                        <button className="name-btn">
+                          <img src={QMark} alt="Question Mark"></img>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <Field
+                    name="goal"
+                    value={goal}
+                    style={{ display: "none" }}
+                  ></Field>
+                  <Goal setFieldValue={setFieldValue} goal={usergoal}></Goal>
+                </div>
+                <div className="buttons-area">
+                  <div>
+                    <button
+                      className="cancel delete"
+                      type="button"
+                      onClick={() => handleDeleteButton()}
+                      // onClick={() => props.handleButton()}
+                    >
+                      Delete
+                    </button>
+                    <button className="cancel" type="button">
+                      Archive
+                    </button>
+                  </div>
+                  <div className="margin">
+                    <button
+                      className="cancel"
+                      onClick={() => props.handleButton()}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="submit"
+                      disabled={isSubmitting}
+                      className="cancel save"
+                    >
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </>
+  );
+}
+EditHabit.prototype = {
+  handleButton: PropTypes.func.isRequired,
+};
+EditHabit.defaultProps = {
+  handleButton: () => {},
+};
+export default EditHabit;
