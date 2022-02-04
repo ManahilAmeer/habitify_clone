02ebba0027f81acf7ee81d39c84a83b17f535a4a
Manahil Ameer <manahil1054@gmail.com>
2022-02-04 12:08:31 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Landing from "@views/Landing/Landing";
 import SignIn from "@views/Sign-in/SignIn";
 import { Routes, Route } from "react-router-dom";
-import SplitPane from "@components/home/Home";
+import Home from "@components/home/Home";
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Landing />}></Route>
-        <Route path="/app" element={<SplitPane />}></Route>
+        <Route path="/app" element={<Home />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
       </Routes>
   );
