66411b0133c761b0501c600eca6d780c0b2ecab2
Manahil Ameer <manahil1054@gmail.com>
2022-02-05 21:26:48 +0500

    Adding secure routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import Landing from "@views/Landing/Landing";
 import SignIn from "@views/Sign-in/SignIn";
 import { Routes, Route } from "react-router-dom";
 import Home from "@components/home/Home";
+import Routers from "routes";
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Landing />}></Route>
-        <Route path="/app" element={<Home />}></Route>
-        <Route path="/sign-in" element={<SignIn />}></Route>
-      </Routes>
+      <Routers />
   );
 }
 export default App;
