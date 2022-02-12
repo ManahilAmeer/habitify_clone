92baeae57e5b7fbf1dec6e106f63f289a6b59e30
Manahil Ameer <manahil1054@gmail.com>
2022-02-12 18:55:35 +0500

    Update Progress.js

--- src/Components/Progress/Progress.js
+++ src/Components/Progress/Progress.js
@@ -1,7 +1,21 @@
 import React from "react";
-import "@components/Progress/progress.css";
-function Progress() {
-  return <div className="progress_bar"></div>;
+import "Components/Progress/progress.css";
+import ProgressHeader from 'Components/ProgressHeader/ProgressHeader'
+import PropTypes from "prop-types";
+import ProgressContent from "Components/ProgressContent/ProgressContent";
+function Progress(props) {
+  const {habitName,CompleteLength,SkipLength,FailLength}=props;
+  return (
+    <div className="progress_bar">
+      <div className="progress-section">
+        <ProgressHeader habitName={habitName}></ProgressHeader>
+        <ProgressContent
+          CompleteLength={CompleteLength}
+          FailLength={FailLength}
+          SkipLength={SkipLength}
+        ></ProgressContent>
+      </div>
+    </div>
+  );
 }
-
 export default Progress;
