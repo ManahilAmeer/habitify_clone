2c8b2eb291e6a0b234c3471962fc09ae3dc60937
Manahil Ameer <manahil1054@gmail.com>
2022-02-12 14:40:57 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import Landing from "@views/Landing/Landing";
-import SignIn from "@views/Sign-in/SignIn";
-import { Routes, Route } from "react-router-dom";
-import Home from "@components/home/Home";
 import Routers from "routes";
 function App() {
   return (
